1837105f751cf12603b967772bf717f51be62a39
chunyu238 <chunyu2382414@gmail.com>
2019-01-09 22:37:45 -0500

    added in board generation and bolding

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class Board extends React.Component {
 
     renderSquare(i) {
         return (
-            <Square
+            <Square key={'square_'+i}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
             />
@@ -22,24 +22,23 @@ class Board extends React.Component {
     }
 
     render() {
+        let board =[];
+        let squares=[];
+        let squareCount = 0;
+        
+        for(let row =0;row<3;row++){
+            for(let col=0;col<3;col++){
+                squares.push(this.renderSquare((squareCount)));
+                squareCount++;
+            }
+            board.push(<div key={'board_'+row} className="board-row">{squares}</div>);
+            squares = [];
+        }
 
         return (
+
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {board}
             </div>
         );
     }
@@ -87,20 +86,42 @@ class Game extends React.Component {
     render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-        
+        const winnerSquares = calculateWinner(current.squares);
+        let winner;
+
+        if(winnerSquares){
+            winner = winnerSquares[0];
+        }else{
+            winner = null;
+        }
+
         const moves = history.map((step,move) => {
             const desc = move ? 'Go to move #' + move : 'Go to game start';
+            let changedIndex = -1;
+            let colRowText;
+            
+            if(move > 0){
+                for(let i=0;i<9;i++){
+                    if(step.squares[i] !== history[move-1].squares[i]){
+                        changedIndex = i;
+                    }
+                }
+            }
+
+            if(changedIndex > -1){
+                colRowText = '(' + Math.floor(changedIndex/3) + ',' + changedIndex%3 + ')';
+            }
+
             return(
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button style={this.state.stepNumber === move ? {fontWeight: 'bold'}: {fontWeight: 'normal'}} onClick={() => this.jumpTo(move)}>{desc} {colRowText}</button>
                 </li>
             ) 
         })
 
         let status;
         if(winner){
-            status = 'Winner: ' + winner;
+            status = 'Winner: ' + current.squares[winner];
         }else if(this.state.stepNumber === 9){
             status = 'Draw: Its a Tie!!';
         }else{
@@ -135,7 +156,7 @@ function calculateWinner(squares){
     for(let i = 0;i<lines.length;i++){
         const [a,b,c] = lines[i];
         if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-            return squares[a];
+            return lines[i];
         }
     }
     return false;
